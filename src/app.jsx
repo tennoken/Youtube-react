9015ad341b5c5a91815623777a05ccce555f04cf
Kzero <41933570+tennoken@users.noreply.github.com>
2021-09-12 17:03:24 +0900

    add youtube service

--- src/app.jsx
+++ src/app.jsx
@@ -3,45 +3,20 @@ import './app.css';
 import Search from './components/video_search/search';
 import VideoList from './components/video_list/videoList';
 
-function App() {
+function App({ youtube }) {
   const [videos, setVideos] = useState([]);
 
-  const onSearch = (value) => {
-    const requestOptions = {
-      method: 'GET',
-      redirect: 'follow',
-    };
-    const popularVideoListApiKey =
-      process.env.REACT_APP_YOUTUBE_POPULAR_VIDEOLIST_API_KEY;
-
-    fetch(
-      `https://youtube.googleapis.com/youtube/v3/search?key=${popularVideoListApiKey}&type=video&part=snippet&q=${value}&chart=mostPopular&maxResults=25`,
-      requestOptions,
-    )
-      .then((response) => response.json())
-      .then((result) =>
-        result.items.map((item) => ({ ...item, id: item.id.videoId })),
-      )
-      .then((items) => setVideos(items))
-      .catch((error) => console.log('error', error));
+  const onSearch = (query) => {
+    youtube
+      .search(query) //
+      .then((item) => setVideos(item));
   };
 
   useEffect(() => {
-    const requestOptions = {
-      method: 'GET',
-      redirect: 'follow',
-    };
-    const popularVideoListApiKey =
-      process.env.REACT_APP_YOUTUBE_POPULAR_VIDEOLIST_API_KEY;
-
-    fetch(
-      `https://youtube.googleapis.com/youtube/v3/videos?key=${popularVideoListApiKey}&part=snippet&chart=mostPopular&maxResults=25`,
-      requestOptions,
-    )
-      .then((response) => response.json())
-      .then((result) => setVideos(result.items))
-      .catch((error) => console.log('error', error));
-  }, []);
+    youtube
+      .mostPopular() //
+      .then((item) => setVideos(item));
+  }, [youtube]);
 
   // console.log(videos);
   return (
---
+++ src/service/Youtube.js
@@ -0,0 +1,33 @@
+class Youtube {
+  constructor(key) {
+    this.key = key;
+    this.getRequestOptions = {
+      method: 'GET',
+      redirect: 'follow',
+    };
+  }
+
+  async mostPopular() {
+    const response = await fetch(
+      `https://youtube.googleapis.com/youtube/v3/videos?key=${this.key}&part=snippet&chart=mostPopular&maxResults=25`,
+      this.getRequestOptions,
+    );
+    const result = await response.json();
+    return result.items;
+  }
+
+  async search(query) {
+    const response = await fetch(
+      `https://youtube.googleapis.com/youtube/v3/search?key=${this.key}&type=video&part=snippet&q=${query}&chart=mostPopular&maxResults=25`,
+      this.getRequestOptions,
+    );
+    const result = await response.json();
+    const items = result.items.map((item) => ({
+      ...item,
+      id: item.id.videoId,
+    }));
+    return items;
+  }
+}
+
+export default Youtube;
