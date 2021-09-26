0738ce35f93c208c4d45158f4c41f01cba30db56
Kzero <41933570+tennoken@users.noreply.github.com>
2021-09-26 17:36:56 +0900

    add axios & media query

--- src/components/video_item/videoItem.jsx
+++ src/components/video_item/videoItem.jsx
@@ -5,7 +5,11 @@ import styles from './videoItem.module.css';
 const VideoItem = memo(({ video, onVideoClick }) => {
   return (
     <li className={styles.video} onClick={() => onVideoClick(video)}>
-      <img src={video.snippet.thumbnails.medium.url} alt="thumbnail" />
+      <img
+        src={video.snippet.thumbnails.medium.url}
+        className={styles.img}
+        alt="thumbnail"
+      />
       <div>
         <p className={styles.title}>{video.snippet.title}</p>
         <p className={styles.channelTitle}>{video.snippet.channelTitle}</p>
--- src/service/Youtube.js
+++ src/service/Youtube.js
@@ -1,28 +1,32 @@
 class Youtube {
-  constructor(key) {
-    this.key = key;
-    this.getRequestOptions = {
-      method: 'GET',
-      redirect: 'follow',
-    };
+  constructor(httpClient) {
+    this.youtube = httpClient;
   }
 
   async mostPopular() {
-    const response = await fetch(
-      `https://youtube.googleapis.com/youtube/v3/videos?key=${this.key}&part=snippet&chart=mostPopular&maxResults=25`,
-      this.getRequestOptions,
-    );
-    const result = await response.json();
-    return result.items;
+    const response = await this.youtube.get('videos', {
+      params: {
+        part: 'snippet',
+        chart: 'mostPopular',
+        maxResults: 25,
+      },
+    });
+
+    return response.data.items;
   }
 
   async search(query) {
-    const response = await fetch(
-      `https://youtube.googleapis.com/youtube/v3/search?key=${this.key}&type=video&part=snippet&q=${query}&chart=mostPopular&maxResults=25`,
-      this.getRequestOptions,
-    );
-    const result = await response.json();
-    const items = result.items.map((item) => ({
+    const response = await this.youtube.get('search', {
+      params: {
+        part: 'snippet',
+        chart: 'mostPopular',
+        type: 'video',
+        maxResults: 25,
+        q: query,
+      },
+    });
+
+    const items = response.data.items.map((item) => ({
       ...item,
       id: item.id.videoId,
     }));
